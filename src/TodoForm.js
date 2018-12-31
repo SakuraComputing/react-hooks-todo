c946ac1e399a9d283930d39e6a395253d60f6ab6
Ewan Bauld <ewan.bauld@gmail.com>
2018-12-31 19:25:31 +0000

    Create TODO app using Hooks (16.7)

---
+++ src/TodoForm.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import TextField from '@material-ui/core/TextField';
+import useInputState from './useInputState';
+
+const TodoForm = ({ saveTodo }) => {
+
+    const { value, reset, onChange } = useInputState('');
+
+    return (
+        <form
+            onSubmit={event => {
+                event.preventDefault();
+
+                saveTodo(value);
+                reset();
+            }}
+        >
+            <TextField
+                variane="outlined"
+                placeholder="Add todo"
+                margin="normal"
+                onChange={onChange}
+                value={value}
+            />
+        </form>
+    )
+}
+
+export default TodoForm
---
+++ src/index.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import Typography from '@material-ui/core/Typography';
+import TodoForm from './TodoForm';
+import TodoList from './TodoList';
+import './styles.css';
+import useTodoState from './useTodoState';
+
+const App = () => {
+
+  const { todos, addTodo, deleteTodo } = useTodoState([]);
+
+  return (
+    <div className="App">
+      <Typography component="h1" variant="h2">
+        Todos
+      </Typography>
+      <TodoForm 
+          saveTodo={todoText => {
+            const trimmedText = todoText.trim();
+
+            if(trimmedText.length > 0) {
+              addTodo(trimmedText)
+            }
+          }} 
+      />
+      <TodoList 
+        todos={todos} 
+        deleteTodo={deleteTodo}
+      />
+    </div>
+  );
+};
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
